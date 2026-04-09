f167e1310d94b20e331a45e0349faddb206b718e
Giovane Alves <giovanealvesm@users.noreply.github.com>
2026-04-09 12:17:27 -0300

    Atualiza URL atual do tunnel da API

--- landing_page_config.js
+++ landing_page_config.js
@@ -1,6 +1,6 @@
 ﻿window.HOME_WASH_CONFIG = {
   crmUrl: "",
-    apiUrl: "https://urw-village-dragon-packing.trycloudflare.com",
-  controlUrl: "https://minor-certificate-bathroom-soundtrack.trycloudflare.com",
-  generatedAt: "2026-04-09T11:55:59-03:00"
+    apiUrl: "https://pretty-costumes-fiction-buildings.trycloudflare.com",
+  controlUrl: "https://offering-letter-science-laboratories.trycloudflare.com",
+  generatedAt: "2026-04-09T12:17:15-03:00"
 };
